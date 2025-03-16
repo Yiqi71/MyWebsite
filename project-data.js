4cf8a36fb9f2173eb6b25adada0688f560b1214f
Yiqi71 <yiqi_c@icloud.com>
2025-03-16 00:24:59 -0400

    1

--- project-data.js
+++ project-data.js
@@ -8,9 +8,13 @@ const projects = {
         detail: `
         <div>
             <p>
-                A website designed to visualize complex quantum concepts, making them accessible and interactive for a broader audience.
-                I was responsible for <strong>front-end development</strong>, coding and building the entire website using modern web technologies. 
-                My work included designing the user interface, ensuring responsive layouts, and integrating interactive features to enhance user engagement.
+                A website designed to visualize complex quantum concepts, 
+                making them accessible and interactive for a broader audience.
+                I was responsible for <strong>front-end development</strong>, 
+                coding and building the entire website using modern web technologies. 
+                My work included designing the user interface, 
+                ensuring responsive layouts, 
+                and integrating interactive features to enhance user engagement.
             </p>
         </div> 
         <div>
@@ -33,11 +37,21 @@ const projects = {
         image: "ProjectPics/data-collector_1.png",
         detail: `
         <div>
-            <p>This is a chrome extension collecting users’ browsing activity, visualizing the data collected, and reminding users of the potential risk of data collection and privacy leak.</p>
+            <p>
+                This is a chrome extension collecting users’ browsing activity, visualizing the data collected, and reminding users of the potential risk of data collection and privacy leak.
+            </p>
         </div>
         <div>
-            <p><a href="https://youtu.be/KSIITh_aVlo" target="_blank" class="underline">Click to watch a video demo</a></p>
-            <p><a href="https://chromewebstore.google.com/detail/a-kind-data-collector/noaeedfgdikfefonldofohifakmjdfoi?hl=en-US&utm_source=ext_sidebar" target="_blank" class="underline"> Link to Chrome Extension Store</a></p>
+            <p>
+                <a href="https://youtu.be/KSIITh_aVlo" target="_blank" class="underline">
+                    Click to watch a video demo
+                </a>
+            </p>
+            <p>
+                <a href="https://chromewebstore.google.com/detail/a-kind-data-collector/noaeedfgdikfefonldofohifakmjdfoi?hl=en-US&utm_source=ext_sidebar" target="_blank" class="underline"> 
+                    Link to Chrome Extension Store
+                </a>
+            </p>
         </div>
         `
     },
@@ -47,7 +61,13 @@ const projects = {
         group: "Group Project",
         date: "2024 Spring",
         image: "ProjectPics/whispers_1.png",
-        detail: "An immersive VR experience where players explore ancient ruins and uncover stories through whispers from the past."
+        detail: `
+        <div>
+            <p>
+                An immersive VR experience where players uncover stories through whispers from the past.
+            </p>
+        </div>
+        `
     },
     "ballance": {
         title: "Ballance",
@@ -55,7 +75,14 @@ const projects = {
         group: "Group Project",
         date: "2024 Fall",
         image: "ProjectPics/ballance_1.png",
-        detail: "An Arduino-powered interactive toy that challenges players to balance elements, encouraging both play and learning about physical balance."
+        detail: `
+        <div>
+            <p>
+                An Arduino-powered interactive toy that challenges players to balance elements, 
+                encouraging both play and learning about physical balance.
+            </p>
+        </div>
+        `
     },
     "light-of-connection": {
         title: "Light of Connection",
@@ -63,7 +90,13 @@ const projects = {
         group: "Group Project",
         date: "2024 Fall",
         image: "ProjectPics/light-of-connection_1.png",
-        detail: "A wearable device using Arduino, designed to light up in response to nearby devices, symbolizing human connections in digital spaces."
+        detail: `
+        <div>
+            <p>
+                A wearable device using Arduino, designed to light up in response to nearby devices, symbolizing human connections in digital spaces.
+            </p>
+        </div>
+        `
     },
     "moons-on-sale": {
         title: "Moons on Sale",
@@ -71,6 +104,110 @@ const projects = {
         group: "Individual Project",
         date: "2024 Fall",
         image: "ProjectPics/moons-on-sale_1.jpg",
-        detail: "An art installation featuring 3D-printed moons, exploring themes of commercialization and our relationship with celestial bodies."
+        detail: `
+        <div>
+            <p>
+                An art installation featuring 3D-printed moons, 
+                exploring themes of commercialization and our relationship with celestial bodies.
+            </p>
+        </div>
+        `
+    },
+
+
+
+
+    "oops": {
+        "title": "Oops, You Died.",
+        "description": "Arduino Interactive Installation",
+        "group": "Group Project",
+        "date": "2024 Fall",
+        "image": "ProjectPics/oops_1.png",
+        "detail": `
+        <div>
+            <p>
+                An interactive installation using Arduino, 
+                where players' actions trigger the box to spell out three coins onto the map, 
+                uncovering how the player died in their last life.
+            </p>
+        </div>
+        `
+    },
+    "life-of-a-drink": {
+        "title": "Life of a Drink",
+        "description": "360 Video",
+        "group": "Group Project",
+        "date": "2024 Spring",
+        "image": "ProjectPics/life_1.png",
+        "detail": `
+        <div>
+            <p>
+                A 360-degree video experience exploring the journey of a drink, 
+                from shop to bin, 
+                offering a unique perspective of daily life.
+            </p>
+        </div>
+        `
+    },
+    "popcorn-rain": {
+        "title": "Popcorn Rain",
+        "description": "p5.js + Tone.js Game",
+        "group": "Group Project",
+        "date": "2024 Spring",
+        "image": "ProjectPics/popcorn-rain_1.png",
+        "detail": `
+        <div>
+            <p>
+                A two-player game made with p5.js and Tone.js, 
+                simulating a rain of popcorn falling over the screen, 
+                merging digital creativity with playful musical interaction.
+            </p>
+        </div>
+        `
+    },
+    "a-plate": {
+        "title": "A Plate",
+        "description": "Plastic Material Exploration",
+        "group": "Individual Project",
+        "date": "2024 Spring",
+        "image": "ProjectPics/plate_1.png",
+        "detail": `
+        <div>
+            <p>
+                An exploration of plastic as a material, 
+                examining its potential for shaping and its impact on environmental sustainability in everyday objects.
+            </p>
+        </div>
+        `
+    },
+    "pAInter": {
+        "title": "pAInter",
+        "description": "UX Design",
+        "group": "Individual Project",
+        "date": "2022 Fall",
+        "image": "ProjectPics/pAInter_1.png",
+        "detail": `
+        <div>
+            <p>
+                A UX design project focused on creating an intuitive interface for a personalized art platform,
+                blending digital art with user interaction.
+            </p>
+        </div>
+        `
+    },
+    "echoes": {
+        "title": "Echoes",
+        "description": "Board Game",
+        "group": "Individual Project",
+        "date": "2025 Spring",
+        "image": "ProjectPics/echoes_1.jpg",
+        "detail": `
+        <div>
+            <p>
+                A board game designed around the concept of memories, 
+                where players try to uncover their forgotten stories through interactions and clues.
+            </p>
+        </div>
+        `
     }
 };
